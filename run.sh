715f2298a87a8bfd8d9259181578e1553f66c14a
Josh Mayer <josh.25.mayer@gmail.com>
2026-02-09 13:09:23 -0500

    simpify shell

--- run.sh
+++ run.sh
@@ -6,15 +6,11 @@ DATA_DIR="${DATA_DIR:-./web}"
 
 if [ "$1" = "download" ]; then
   mkdir -p "$DATA_DIR"
-  gsutil -m cp -r "gs://${BUCKET}/web/*" "$DATA_DIR/" 2>/dev/null || \
-  gcloud storage cp -r "gs://${BUCKET}/web/*" "$DATA_DIR/" || \
-  (echo "Error: need gsutil or gcloud" && exit 1)
+  gsutil -m cp -r "gs://${BUCKET}/web/*" "$DATA_DIR/"
 elif [ "$1" = "run" ]; then
-  python hw2.py --data-dir "$DATA_DIR"
+  python -u hw2.py --data-dir "$DATA_DIR"
 else
   mkdir -p "$DATA_DIR"
-  gsutil -m cp -r "gs://${BUCKET}/web/*" "$DATA_DIR/" 2>/dev/null || \
-  gcloud storage cp -r "gs://${BUCKET}/web/*" "$DATA_DIR/" || \
-  (echo "Error: need gsutil or gcloud" && exit 1)
-  python hw2.py --data-dir "$DATA_DIR"
+  gsutil -m cp -r "gs://${BUCKET}/web/*" "$DATA_DIR/"
+  python -u hw2.py --data-dir "$DATA_DIR"
 fi

2840cf8b2461a6077885b90f2a1c6ea603433d36
Josh Mayer <josh.25.mayer@gmail.com>
2026-02-28 22:18:54 -0500

    vm1 workig

--- hwk4/first_service/startup-server.sh
+++ hwk4/first_service/startup-server.sh
@@ -7,14 +7,16 @@ PROJECT_ID="cs528-jm"
 GIT_REPO_URL="https://github.com/joshmayerr/jweb.git"
 
 apt-get update -y
-apt-get install -y python3 python3-pip git
+apt-get install -y python3 python3-venv git
 
 export GOOGLE_CLOUD_PROJECT="$PROJECT_ID"
 export BUCKET="jweb-content"
 export FORBIDDEN_TOPIC="jweb-forbidden"
+export PORT="80"
 
 git clone "$GIT_REPO_URL" /tmp/jweb || { echo "Clone failed."; exit 1; }
 cd /tmp/jweb/hwk4/first_service
-pip3 install -r requirements.txt
-nohup python3 server.py </dev/null >>/var/log/jweb-server.log 2>&1 &
+python3 -m venv /opt/jweb-venv
+/opt/jweb-venv/bin/pip install -r requirements.txt
+nohup /opt/jweb-venv/bin/python server.py </dev/null >>/var/log/jweb-server.log 2>&1 &
 exit 0
